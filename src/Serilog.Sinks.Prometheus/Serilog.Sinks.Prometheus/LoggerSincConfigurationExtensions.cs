a1e129a16c1fd11c949396b40666627aae71ce7c
Зверев Иван <i.zverev@dodopizza.com>
2018-08-07 17:17:11 +0300

    added LogEventSink for prometheus

---
+++ src/Serilog.Sinks.Prometheus/Serilog.Sinks.Prometheus/LoggerSincConfigurationExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Serilog.Configuration;
+
+namespace Serilog.Sinks.Prometheus
+{
+	public  static class LoggerSincConfigurationExtensions
+	{
+		public static LoggerConfiguration Prometheus(this LoggerSinkConfiguration sinkConfiguration, string counterNameTemplate, string counterDescription)
+		{
+			return sinkConfiguration.Sink(new PrometheusEventSink(counterNameTemplate, counterDescription));
+		}
+	}
+}
---
+++ src/Serilog.Sinks.Prometheus/Serilog.Sinks.Prometheus/PrometheusEventSink.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Concurrent;
+using Prometheus;
+using Serilog.Core;
+using Serilog.Events;
+
+namespace Serilog.Sinks.Prometheus
+{
+	internal class PrometheusEventSink : ILogEventSink
+	{
+		private readonly string _counterNameTemplate;
+		private readonly string _counterDescription;
+		private readonly ConcurrentDictionary<string, Counter> _countersCache = new ConcurrentDictionary<string, Counter>();
+
+		public PrometheusEventSink(string counterNameTemplate, string counterDescription)
+		{
+			_counterNameTemplate = counterNameTemplate;
+			_counterDescription = counterDescription;
+		}
+
+		public void Emit(LogEvent logEvent)
+		{
+			var counterName = string.Format(_counterNameTemplate, logEvent.Level.ToString().ToLower());
+			var counterDescription = string.Format(_counterDescription, logEvent.Level);
+
+			var counter = _countersCache.GetOrAdd(counterName, n => Metrics.CreateCounter(counterName, counterDescription));
+			counter.Inc();
+		}
+	}
+}
